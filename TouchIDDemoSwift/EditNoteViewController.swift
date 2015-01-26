9a1a4bd4d66e4292caf138b2da1320486cfc68c8
“KEEVIN <keevin.mitchell@gmail.com>
2015-01-26 14:59:12 -0500

    Added new view controller

---
+++ TouchIDDemoSwift/EditNoteViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EditNoteViewController.swift
+//  TouchIDDemoSwift
+//
+//  Created by KEEVIN MITCHELL on 1/26/15.
+//  Copyright (c) 2015 Beyond 2021. All rights reserved.
+//
+
+import UIKit
+
+class EditNoteViewController: UIViewController {
+
+}
