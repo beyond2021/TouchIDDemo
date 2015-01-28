936c3dcd14d1769bfc2a5bb35d7b708dc872b780
“KEEVIN <keevin.mitchell@gmail.com>
2015-01-27 21:20:14 -0500

    Set up user interface

--- TouchIDDemoSwift/EditNoteViewController.swift
+++ TouchIDDemoSwift/EditNoteViewController.swift
@@ -2,12 +2,39 @@
 //  EditNoteViewController.swift
 //  TouchIDDemoSwift
 //
-//  Created by KEEVIN MITCHELL on 1/26/15.
+//  Created by KEEVIN MITCHELL on 1/27/15.
 //  Copyright (c) 2015 Beyond 2021. All rights reserved.
 //
 
 import UIKit
 
 class EditNoteViewController: UIViewController {
+    
+    @IBOutlet weak var txtNoteTitle: UITextField!
+    
+    @IBOutlet weak var tvNoteBody: UITextView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Edit Note"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
