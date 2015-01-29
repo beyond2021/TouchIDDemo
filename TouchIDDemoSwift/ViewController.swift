a7bdc26d68b67830942c15be88276bfbd136a02d
“KEEVIN <keevin.mitchell@gmail.com>
2015-01-29 15:17:40 -0500

    Implementing the TouchID Authentication

--- TouchIDDemoSwift/ViewController.swift
+++ TouchIDDemoSwift/ViewController.swift
@@ -7,22 +7,95 @@
 //
 
 import UIKit
+import LocalAuthentication
+import iAd
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIAlertViewDelegate {
     
     @IBOutlet weak var tblNotes: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.canDisplayBannerAds = true
+        authenticateUser()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    //In here we will write the needed code for integrating the TouchID authentication. As you see, I set no return type to the method, as it’s a void one. Also, it doesn’t accept any parameters at all.
+    
+    func authenticateUser() {
+        // Get the local authentication context.
+        let context : LAContext = LAContext() // or let context = LAContext()
+        
+        // Declare a NSError variable. Notice that the error variable is declared as an optional, because if there’s no error it will be nil. nil in Swift is different from the nil in Objective-C, and it means that there’s no value at all.
+        var error: NSError?
+        
+        // Set the reason string that will appear on the authentication alert.
+        var reasonString = "Authentication is needed to access your notes."
+        
+        //The next step is to ask the framework if the TouchID authentication can be applied to the specific device, by calling a special function named canEvaluatePolicy. It accepts two parameters, the policy we want to evaluate and an error object. Here’s how it’s used:
+        // Check if the device can evaluate the policy.
+        if context.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            
+            //The DeviceOwnerAuthenticationWithBiometrics is a property of the LAPolicy class. Note that the error variable is passed by reference. If this condition is true, then the device supports the TouchID authentication, the TouchID mechanism has been enabled in the device’s Settings, a passcode has been also set, and of course, one finger at least has been enrolled. That means that the specific authentication policy can be applied, and the TouchID authentication dialog to appear:
+            
+            [context .evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString, reply: { (success: Bool, evalPolicyError: NSError?) -> Void in
+                
+                if success {
+                    
+                }
+                else{
+                    // If authentication failed then show a message to the console with a short description.
+                    // In case that the error is a user fallback, then show the password alert view.
+                    println(evalPolicyError?.localizedDescription)
+                    
+                    switch evalPolicyError!.code {
+                        
+                    case LAError.SystemCancel.rawValue:
+                        println("Authentication was cancelled by the system")
+                        
+                    case LAError.UserCancel.rawValue:
+                        println("Authentication was cancelled by the user")
+                        
+                    case LAError.UserFallback.rawValue:
+                        println("User selected to enter custom password")
+                        NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                            self.showPasswordAlert()
+                        })
+                        
+                    default:
+                        println("Authentication failed")
+                        NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                            self.showPasswordAlert()
+                        })
+                    }
+                }
+                
+                
+                
+            })]
+            
+        }
+        
+    }
+    
+    
+    func showPasswordAlert() {
+        var passwordAlert : UIAlertView = UIAlertView(title: "TouchIDDemo", message: "Please type your password", delegate: self, cancelButtonTitle: "Cancel", otherButtonTitles: "Okay")
+        passwordAlert.alertViewStyle = UIAlertViewStyle.SecureTextInput
+        passwordAlert.show()
+    }
+    
+    
+    
 
 }
 
