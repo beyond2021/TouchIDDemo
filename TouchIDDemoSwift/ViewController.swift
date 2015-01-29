2492ce5ebd12ccf93c57919eafa74af2faf2cfdb
“KEEVIN <keevin.mitchell@gmail.com>
2015-01-29 17:06:10 -0500

    Listing Notes

--- TouchIDDemoSwift/ViewController.swift
+++ TouchIDDemoSwift/ViewController.swift
@@ -10,14 +10,43 @@ import UIKit
 import LocalAuthentication
 import iAd
 
-class ViewController: UIViewController, UIAlertViewDelegate {
+class ViewController: UIViewController, UIAlertViewDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tblNotes: UITableView!
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate ////declaring and instantiating at the same time an application delegate constant.
+    
+    
+    var dataArray: NSMutableArray! //we need an array (a NSMutableArray) that will be used as a data container. If you’re wondering why a mutable array and not an immutable one, then I must say because later we’ll implement another feature for deleting notes, and we’ll need to modify the array’s contents.
+    //Note that the array has been declared as an optional, because if no data file exists, the array will remain nil.
+    
+    
+    func loadData(){
+        if appDelegate.checkIfDataFileExists() {
+            
+            // If the data file exists, then we load its contents to the array and we reload the tableview, otherwise we just display a message to the console.
+            
+            self.dataArray = NSMutableArray(contentsOfFile: appDelegate.getPathOfDataFile())
+            self.tblNotes.reloadData()
+        }
+        else{
+            println("File does not exist")
+        }
+        //By having the above method ready, we can go and call it wherever it’s needed. Let’s begin by going to the authenticateUser method, in the completion handler block and in the successful authentication case. Also, let’s call it when the correct password is typed in the alert view:
+        
+    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.canDisplayBannerAds = true
+        
+        
+        tblNotes.delegate = self
+        tblNotes.dataSource = self
+        
+        
         authenticateUser()
         
     }
@@ -50,9 +79,11 @@ class ViewController: UIViewController, UIAlertViewDelegate {
             [context .evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString, reply: { (success: Bool, evalPolicyError: NSError?) -> Void in
                 
                 if success {
-                    
-                }
-                else{
+                    //here we call the l;oaddata method
+                    NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                        self.loadData()
+                    })
+                }                else{
                     // If authentication failed then show a message to the console with a short description.
                     // In case that the error is a user fallback, then show the password alert view.
                     println(evalPolicyError?.localizedDescription)
@@ -118,6 +149,8 @@ class ViewController: UIViewController, UIAlertViewDelegate {
             if !alertView.textFieldAtIndex(0)!.text.isEmpty {
                 if alertView.textFieldAtIndex(0)!.text == "appcoda" {
                     
+                    loadData()
+                    
                 }
                 else{
                     showPasswordAlert()
@@ -138,6 +171,70 @@ class ViewController: UIViewController, UIAlertViewDelegate {
     */
     
     
-
+   // MARK: - TableView Datasource Methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        //At first, we must specify the number of sections in the tableview:
+        
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let array = dataArray {
+            //we must return the proper number of rows. Remember at this point that if the notes data file doesn’t exist, the dataArray mutable array we declared earlier won’t get initialised and it will remain nil. So, we must be sure first that the array is not nil and return the proper number of rows, otherwise we must return 0.
+            //As you see, if the dataArray actually exists, we unwrap it to the array constant and we return the total number of its objects
+            
+            
+            return array.count
+        }
+        else{
+            return 0
+        }
+    }
+    
+    //Alternate numberOfRows
+    /*func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return dataArray.count
+    
+    … as long as you don’t declare the dataArray as an optional, but you initialize it upon its declaration so it will never be nil. So, instead of writing this…
+    1
+    
+    var dataArray: NSMutableArray!
+    
+    … you just need to write this:
+    1
+    
+    var dataArray: NSMutableArray = NSMutableArray()
+    
+    However, this approach would make the dataArray to be initialised once again with the contents of the file in the loadData method, but furthermore, our initial implementation consists of a nice chance to work with optionals!
+    */
+    
+    
+    
+    //Our next step is to return a cell:
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        //After having dequeued the cell, we assign the dictionary with each note data to the currentNote constant. Next, we just get the each note’s title and we set it as the text to the cell’s label.
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("idCell") as UITableViewCell
+        
+        let currentNote = self.dataArray.objectAtIndex(indexPath.row) as Dictionary<String, String>
+        cell.textLabel.text = currentNote["title"]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    
+    
+    
+    
 }
+
+
+
 
